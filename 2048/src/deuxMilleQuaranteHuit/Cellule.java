39354c3deee2f83596203392d6b956537133c64c
amesle <amesle@SLAM01.sio.cdfpedago.local>
2014-10-06 13:09:51 +0200

    * first commit

---
+++ 2048/src/deuxMilleQuaranteHuit/Cellule.java
@@ -0,0 +1,6 @@
+package deuxMilleQuaranteHuit;
+
+public class Cellule 
+{
+	private int ligne, colonne;
+}
---
+++ 2048/src/deuxMilleQuaranteHuit/Grille.java
@@ -0,0 +1,6 @@
+package deuxMilleQuaranteHuit;
+
+public class Grille 
+{
+	private int nbLignes, nbColonnes;
+}
